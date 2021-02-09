38df0c41e4fdc1b0d6606c8b770ddecb87a94857
Elysiayn <wendy.lemus.wl@gmail.com>
2021-02-08 22:21:55 -0500

    merge from feature/comments

--- models/index.js
+++ models/index.js
@@ -1,9 +1,60 @@
-const router = require('express').Router();
+// import all models
+const Post = require('./Post');
+const User = require('./User');
+const Vote = require('./Vote');
+const Comment = require('./Comment');
 
-const userRoutes = require('./user-routes.js');
-const postRoutes = require('./post-routes');
+// create associations
+User.hasMany(Post, {
+  foreignKey: 'user_id'
+});
 
-router.use('/users', userRoutes);
-router.use('/posts', postRoutes);
+Post.belongsTo(User, {
+  foreignKey: 'user_id'
+});
 
-module.exports = router;
+User.belongsToMany(Post, {
+  through: Vote,
+  as: 'voted_posts',
+  foreignKey: 'user_id'
+});
+
+Post.belongsToMany(User, {
+  through: Vote,
+  as: 'voted_posts',
+  foreignKey: 'post_id'
+});
+
+Vote.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Vote.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+User.hasMany(Vote, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Vote, {
+  foreignKey: 'post_id'
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Vote, Comment };
